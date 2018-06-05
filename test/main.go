5481b22a0e352b7a4ff226b69bd4754d73aeb1e0
zhaifei <zhaifei@hellobike.com>
2018-06-05 18:09:45 +0800

    format test script

---
+++ test/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/philchia/agollo"
+)
+
+func main() {
+	if err := agollo.StartWithConfFile("../testdata/app.properties"); err != nil {
+		log.Fatal(err)
+	}
+
+	defer agollo.Stop()
+
+	changes := agollo.WatchUpdate()
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			timeout := agollo.GetStringValue("timeout", "default")
+			fmt.Println("timeout is:", timeout)
+			fmt.Println("null value is", agollo.GetStringValue("null", "null"))
+			fmt.Println("Client.json is", agollo.GetStringValueWithNameSapce("Client.json", "content", "null"))
+		}
+	}()
+	for change := range changes {
+		if change != nil {
+			fmt.Println(change.Namespace)
+			for k, change := range change.Changes {
+				fmt.Println("change", k, "type", change.ChangeType, "val", change.NewValue)
+			}
+		}
+	}
+}
