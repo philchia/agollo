0f53f02780ca94566933db4931820ce2b99b30f3
zhaifei <zhaifei@hellobike.com>
2018-06-07 15:24:52 +0800

    test notification repo

---
+++ notification_test.go
@@ -0,0 +1,21 @@
+package agollo
+
+import "testing"
+
+func TestNotification(t *testing.T) {
+	repo := new(notificatonRepo)
+
+	repo.setNotificationID("namespace", 1)
+	if id, ok := repo.getNotificationID("namespace"); !ok || id != 1 {
+		t.FailNow()
+	}
+
+	repo.setNotificationID("namespace", 2)
+	if id, ok := repo.getNotificationID("namespace"); !ok || id != 2 {
+		t.FailNow()
+	}
+
+	if str := repo.toString(); str == "" {
+		t.FailNow()
+	}
+}
