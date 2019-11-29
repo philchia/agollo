f66eb6380d13b8e0af1a7aeb017565e20e49b5a3
phil <zgrubby@gmail.com>
2019-11-29 16:38:18 +0800

    rm log

--- conf.go
+++ conf.go
@@ -2,7 +2,6 @@ package agollo
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 )
 
@@ -19,7 +18,6 @@ type Conf struct {
 func NewConf(name string) (*Conf, error) {
 	f, err := os.Open(name)
 	if err != nil {
-		fmt.Println("err:", err)
 		return nil, err
 	}
 	defer f.Close()
