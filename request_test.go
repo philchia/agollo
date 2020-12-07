0e1571af01b3aa861031be8b47ab60063b09c21e
phil <zgrubby@gmail.com>
2020-12-07 11:59:10 +0800

    test request with signature

--- request_test.go
+++ request_test.go
@@ -44,3 +44,43 @@ func TestRequest(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestRequestWithSign(t *testing.T) {
+	request := newHttpSignRequester(newSignature("appid", "secret"), &http.Client{})
+
+	serv := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		auth := req.Header.Get(signHttpHeaderAuthorization)
+		t.Log(auth)
+		rw.Write([]byte("test"))
+	}))
+
+	bts, err := request.request(serv.URL)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !bytes.Equal(bts, []byte("test")) {
+		t.FailNow()
+	}
+
+	serv = httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusInternalServerError)
+	}))
+	bts, err = request.request(serv.URL)
+	if err != nil && err != ErrorStatusNotOK {
+		t.Error(err)
+	}
+
+	if len(bts) != 0 {
+		t.FailNow()
+	}
+
+	serv = httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.WriteHeader(http.StatusInternalServerError)
+	}))
+	serv.Close()
+	_, err = request.request(serv.URL)
+	if err == nil {
+		t.FailNow()
+	}
+}
